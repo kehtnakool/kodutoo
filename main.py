d25140aca2dbdd53ba2a4d59e1d3ffb599447c9e
kehtnakool <peeter.ivask@kehtna.edu.ee>
2023-01-12 17:48:25 +0200

    kodutöö valmis

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.8
+from Controller import Controller
+
+
+class Word_puzzle:
+
+    def __init__(self):
+        Controller().main()
+
+
+if __name__ == '__main__':
+    Word_puzzle()
